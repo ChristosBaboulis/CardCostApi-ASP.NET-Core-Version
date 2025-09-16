348fded3731366737942e81ef7ce666a7e1ca702
ChristosBaboulis <chrisb2603@gmail.com>
2025-09-16 23:06:07 +0300

    Fixes on DTO

--- CardCostApplication/Contracts/Responses/ClearingCostRepresentation.cs
+++ CardCostApplication/Contracts/Responses/ClearingCostRepresentation.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CardCostApplication.Contracts.Responses
 {
 	public class ClearingCostRepresentation
 	{
+		public long Id { get; set; }
+
+		[Required]
+		[MinLength(1)]
 		public string CountryCode { get; set; } = string.Empty;
+
+		[Required]
+		[Range(0, int.MaxValue)]
 		public int Cost { get; set; }
 	}
 }
